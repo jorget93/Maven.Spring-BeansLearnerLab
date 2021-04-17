afabc2df31a274ef59c7a4d62d3a68cec5674b5d
jorge <jorgetapia0293@gmail.com>
2021-04-17 16:41:09 -0400

    finished people class

--- demo/src/main/java/com/example/demo/Instructor.java
+++ demo/src/main/java/com/example/demo/Instructor.java
@@ -2,17 +2,18 @@ package com.example.demo;
 
 public class Instructor extends Person implements Teacher{
 
-
-
     public Instructor(Long id, String name) {
         super(id, name);
     }
 
     public void teach(Learner learner, double numberOfHours) {
-
+        learner.learn(numberOfHours);
     }
 
     public void lecture(Iterable<? extends Learner> learners, double numberOfHours) {
-        double numberOfHoursPerLearner = numberOfHours / learners.length;
+        int i = 0;
+        for(Learner l: learners){ i++; }
+        double numberOfHoursPerLearner = numberOfHours / i;
+        for(Learner l: learners) { l.learn(numberOfHoursPerLearner); }
     }
 }
--- demo/src/main/java/com/example/demo/People.java
+++ demo/src/main/java/com/example/demo/People.java
@@ -1,16 +1,45 @@
 package com.example.demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public abstract class People <PersonType extends Person> implements Iterable <PersonType>{
 
-    private List<Person> personList = new ArrayList<>();
+    private List<PersonType> personList;
 
-    public People(List<Person> list){
+    public People(List<PersonType> list){
         this.personList = list;
     }
 
+    public People(PersonType... person){
+        personList = new ArrayList<>();
+        personList.addAll(Arrays.asList(person));
+    }
 
+    public void add(PersonType p) { personList.add(p); }
 
+    public void remove(PersonType p) { personList.remove(p); }
+
+    public int size(){ return personList.size(); }
+
+    public void clear(){ personList.removeAll(personList); }
+
+    public void addAll(Iterable<? extends PersonType> types){
+        for(PersonType p: types){ personList.add(p); }
+    }
+
+    public PersonType findById(long id){
+        for(PersonType p: personList){
+            if(p.getId().equals(id)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public List<PersonType> findAll(){
+        return personList;
+    }
 }
